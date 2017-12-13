c22fd62e003f5511c69831058ee68610ce384a0d
C4risrocks <c4risrocks@hotmail.com>
2017-12-13 08:35:09 -0600

    Login, ya arreglado
    Falta pulir Login

--- ProyectoErik/app/src/main/java/OpenHelper/SQLite_OpenHelper.java
+++ ProyectoErik/app/src/main/java/OpenHelper/SQLite_OpenHelper.java
@@ -2,8 +2,11 @@ package OpenHelper;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
+import java.sql.SQLException;
 
 /**
  * Created by christianmorenomarin on 12/12/17.
@@ -12,7 +15,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class SQLite_OpenHelper extends SQLiteOpenHelper{
 
     final String query = "create table usuarios (_ID integer primary key autoincrement, " +
-            "Nombres text, Apellidos text, Direccion text, Telefono text, Nombre_Usuario, Password);";
+            "Nombres text, Apellidos text, Direccion text, Telefono text, Nombre_Usuario text, Password text);";
 
 
     public SQLite_OpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -56,6 +59,19 @@ public class SQLite_OpenHelper extends SQLiteOpenHelper{
         valores.put("Nombre_Usuario",nom_usuario);
         valores.put("Password", password);
         this.getWritableDatabase().insert("usuarios", null, valores);
+    }
+
+    //Metodo para validar usuario
+
+    public Cursor ConsultaPassUsuario (String usuario, String password) throws SQLException{
+
+        Cursor mcursor = null;
+
+        mcursor = this.getReadableDatabase().query("usuarios", new String[]{"_ID",
+                "Nombres" , "Apellidos" , "Direccion" , "Telefono" , "Nombre_Usuario" , "Password"}, "Nombre_Usuario like '"
+                +usuario+"' and Password like '"+password+"'",null,null,null,null);
+
+        return mcursor;
     }
 
 }
--- ProyectoErik/app/src/main/java/com/example/christianmorenomarin/proyectoerik/MainActivity.java
+++ ProyectoErik/app/src/main/java/com/example/christianmorenomarin/proyectoerik/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.christianmorenomarin.proyectoerik;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.SQLException;
+
+import OpenHelper.SQLite_OpenHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView tv_registrar;
-    Button btn_registrar;
+    Button btn_entrar;
+    SQLite_OpenHelper helper = new SQLite_OpenHelper(this, "BDusuarios", null, 1);
 
 
     @Override
@@ -18,13 +26,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_registrar = (Button) findViewById(R.id.btn_entrar);
+        btn_entrar = (Button) findViewById(R.id.btn_EntrarM);
 
-        btn_registrar.setOnClickListener(new View.OnClickListener() {
+        btn_entrar.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
-                Intent intentEntrar = new Intent(MainActivity.this, Usuario.class);
-                MainActivity.this.startActivity(intentEntrar);
+
+                EditText editTextUsuarioM = (EditText) findViewById(R.id.mainUsuario);
+                EditText editTextPasswordM = (EditText) findViewById(R.id.mainContrasena);
+
+                try {
+                    Cursor cursor = helper.ConsultaPassUsuario(editTextUsuarioM.getText().toString(), editTextPasswordM.getText().toString());
+                    if(cursor.getCount() > 0){
+                        Intent intentEntrar = new Intent(MainActivity.this, Usuario.class);
+                        MainActivity.this.startActivity(intentEntrar);
+                    }else {
+                        Toast.makeText(getApplicationContext(), "Usuario o Contraseña Incorrecto",
+                                Toast.LENGTH_LONG).show();
+                    }
+
+                    editTextUsuarioM.setText("");
+                    editTextPasswordM.setText("");
+                    editTextUsuarioM.findFocus();
+
+
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
+
             }
         });
 
